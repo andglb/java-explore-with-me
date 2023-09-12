265e1ab0d0e1240e56ca7b641e67c9814ae6e442
Андрей <andrew-g0lub@yandex.ru>
2023-09-12 21:21:17 +0300

    Диплом 1 часть

--- stats/stats-server/src/main/java/ru/practicum/stats_server/StatsServer.java
+++ stats/stats-server/src/main/java/ru/practicum/stats_server/StatsServer.java
@@ -3,9 +3,6 @@ package ru.practicum.stats_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class StatsServer {
