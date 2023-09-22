c2b81c9a2939452974809748900612d74fad5c8f
Андрей <andrew-g0lub@yandex.ru>
2023-09-22 21:59:50 +0300

    Диплом 2 часть

--- main-service/src/main/java/ru/practicum/main_service/dto/category/CategoryResponseDto.java
+++ main-service/src/main/java/ru/practicum/main_service/dto/category/CategoryResponseDto.java
@@ -4,10 +4,15 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
 @Getter
 @Setter
 @Builder
 public class CategoryResponseDto {
     private Long id;
+    @NotBlank
+    @Size(min = 1, max = 50)
     private String name;
 }
