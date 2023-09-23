c53dd05cdf402296f231adbf96ff03260b3ce214
Андрей <andrew-g0lub@yandex.ru>
2023-09-24 00:43:47 +0300

    Диплом 2 часть

--- main-service/src/main/java/ru/practicum/main_service/exceptions/ErrorHandler.java
+++ main-service/src/main/java/ru/practicum/main_service/exceptions/ErrorHandler.java
@@ -140,11 +140,11 @@ public class ErrorHandler {
     }
 
     @ExceptionHandler
-    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ResponseStatus(HttpStatus.CONFLICT)
     @ResponseBody
     public ApiError handleCompilationNotExistException(final CompilationNotExistException exception) {
         return new ApiError("Can't delete user with this id", "User with this id doesn't exist",
-                HttpStatus.NOT_FOUND.getReasonPhrase().toUpperCase(), LocalDateTime.now().format(dateFormatter));
+                HttpStatus.CONFLICT.getReasonPhrase().toUpperCase(), LocalDateTime.now().format(dateFormatter));
     }
 
     @ExceptionHandler
