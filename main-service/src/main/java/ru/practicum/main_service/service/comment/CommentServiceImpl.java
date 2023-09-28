e3d4f54564095f52f680bb3d9028d96308468ebd
Андрей <andrew-g0lub@yandex.ru>
2023-09-28 22:40:34 +0300

    Диплом 3 часть (доработка)

--- main-service/src/main/java/ru/practicum/main_service/service/comment/CommentServiceImpl.java
+++ main-service/src/main/java/ru/practicum/main_service/service/comment/CommentServiceImpl.java
@@ -33,12 +33,12 @@ import java.util.List;
 @RequiredArgsConstructor
 @Slf4j
 public class CommentServiceImpl implements CommentService {
-    private final CommentRepository commentsRepository;
+    private final CommentRepository commentRepository;
     private final UserRepository userRepository;
     private final EventRepository eventRepository;
     private final CommentMapper commentMapper;
     private final EntityManager entityManager;
-    public static final String COMMENT_WAS_UPD = "Comment with ID = {} was update";
+    public static final String COMMENT_WAS_UPDATE = "Comment with ID = {} was update";
 
     @Override
     public CommentDto createComment(NewCommentDto newCommentDto, Long userId, Long eventId) {
@@ -52,37 +52,37 @@ public class CommentServiceImpl implements CommentService {
         comment.setEvent(event);
         comment.setCreated(LocalDateTime.now());
         comment.setText(newCommentDto.getText());
-        return commentMapper.toCommentDto(commentsRepository.save(comment));
+        return commentMapper.toCommentDto(commentRepository.save(comment));
     }
 
     @Override
     @Transactional
     public CommentDto updateCommentByUser(NewCommentDto newCommentDto, Long userId, Long commentId) {
-        Comment oldComment = commentsRepository.findById(commentId).orElseThrow(() -> new CommentNotExistException("Can't update comment, comment doesn't exist"));
+        Comment oldComment = commentRepository.findById(commentId).orElseThrow(() -> new CommentNotExistException("Can't update comment, comment doesn't exist"));
         if (!userRepository.existsById(userId)) {
-            throw new UserNotExistException("Can't delete comment, if user doesn't exist");
+            throw new UserNotExistException("Can't update comment, if user doesn't exist");
         }
         if (!oldComment.getAuthor().getId().equals(userId)) {
-            throw new CommentConflictException("Can't delete comment, if his owner another user");
+            throw new CommentConflictException("Can't update comment, if his owner another user");
         }
         oldComment.setText(newCommentDto.getText());
-        Comment savedComment = commentsRepository.save(oldComment);
-        log.debug(COMMENT_WAS_UPD, commentId);
+        Comment savedComment = commentRepository.save(oldComment);
+        log.debug(COMMENT_WAS_UPDATE, commentId);
         return commentMapper.toCommentDto(savedComment);
     }
 
     @Override
     public CommentDto updateCommentByAdmin(NewCommentDto newCommentDto, Long commentId) {
-        Comment oldComment = commentsRepository.findById(commentId).orElseThrow(() -> new CommentNotExistException("Can't update comment, comment doesn't exist"));
+        Comment oldComment = commentRepository.findById(commentId).orElseThrow(() -> new CommentNotExistException("Can't update comment, comment doesn't exist"));
         oldComment.setText(newCommentDto.getText());
-        Comment savedComment = commentsRepository.save(oldComment);
-        log.debug(COMMENT_WAS_UPD, commentId);
+        Comment savedComment = commentRepository.save(oldComment);
+        log.debug(COMMENT_WAS_UPDATE, commentId);
         return commentMapper.toCommentDto(savedComment);
     }
 
     @Override
     public CommentDto getCommentsByIdByUser(Long userId, Long commentId) {
-        Comment comment = commentsRepository.findById(commentId).orElseThrow(() -> new CommentNotExistException("Can't get comment"));
+        Comment comment = commentRepository.findById(commentId).orElseThrow(() -> new CommentNotExistException("Can't get comment"));
         if (!userRepository.existsById(userId)) {
             throw new UserNotExistException("Can't get comment, if user doesn't exist");
         }
@@ -96,17 +96,16 @@ public class CommentServiceImpl implements CommentService {
 
     @Override
     public List<CommentDto> getUserCommentsByCreateTime(Long userId, LocalDateTime createStart, LocalDateTime createEnd, Integer from, Integer size) {
-        userRepository.findById(userId);
+        userRepository.findById(userId)
+                .orElseThrow(() -> new UserNotExistException(String.format("Can't create event, the user with id = %s doesn't exist", userId)));
 
         CriteriaBuilder builder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Comment> query = builder.createQuery(Comment.class);
 
         Root<Comment> root = query.from(Comment.class);
 
-        if (createStart != null && createEnd != null) {
-            if (createEnd.isBefore(createStart)) {
-                throw new WrongTimeException("createEnd must be after createStart");
-            }
+        if (createEnd.isBefore(createStart)) {
+            throw new WrongTimeException("createEnd must be after createStart");
         }
 
         Predicate criteria = root.get("author").in(userId);
@@ -131,7 +130,8 @@ public class CommentServiceImpl implements CommentService {
 
     @Override
     public void deleteCommentByUser(Long userId, Long commentId) {
-        Comment comment = commentsRepository.findById(commentId).orElseThrow(() -> new CommentNotExistException("Can't delete comment, if his owner another user or user/comment doesn't exist"));
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new CommentNotExistException("Can't delete comment, if his owner another user or user/comment doesn't exist"));
         if (!userRepository.existsById(userId)) {
             throw new UserNotExistException("Can't delete comment, if user doesn't exist");
         }
@@ -140,28 +140,31 @@ public class CommentServiceImpl implements CommentService {
             throw new CommentConflictException("Can't delete comment, if his owner another user");
         }
         log.debug("Comment with ID = {} was delete", commentId);
-        commentsRepository.delete(comment);
+        commentRepository.delete(comment);
     }
 
     @Override
     public List<CommentDto> getCommentsByEventIdByAdmin(Long eventId, Integer from, Integer size) {
         eventRepository.findById(eventId).orElseThrow(() -> new EventNotExistException(String.format("Can't receive comment by id, event with id=%s doesn't exist", eventId)));
         Pageable page = PageRequest.of(from / size, size);
-        List<Comment> eventComments = commentsRepository.findAllByEventId(eventId, page);
+        List<Comment> eventComments = commentRepository.findAllByEventId(eventId, page);
         log.debug("Get comment`s list of event with ID = {}", eventId);
         return commentMapper.toCommentDtos(eventComments);
     }
 
     @Override
     public CommentDto getCommentsByIdByAdmin(Long commentId) {
-        Comment comment = commentsRepository.findById(commentId).orElseThrow(() -> new CommentNotExistException(String.format("Can't receive comment by id, the comment with id=%s doesn't exist", commentId)));
+        Comment comment = commentRepository.findById(commentId)
+                .orElseThrow(() -> new CommentNotExistException(String.format("Can't receive comment by id, the comment with id=%s doesn't exist", commentId)));
         log.debug("Comment with ID = {} was found", commentId);
         return commentMapper.toCommentDto(comment);
     }
 
     @Override
     public void deleteCommentByAdmin(Long commentId) {
-        commentsRepository.deleteById(commentId);
+        commentRepository.findById(commentId)
+                .orElseThrow(() -> new CommentNotExistException(String.format("Can't delete comment, the comment with id=%s doesn't exist", commentId)));
+        commentRepository.deleteById(commentId);
         log.debug("Comment with ID = {} was delete", commentId);
     }
 }
